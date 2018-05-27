435d3cd1e20593119cbf26b838610aabef333c16
dfnino10 <df.nino10@uniandes.edu.co>
2018-05-26 20:35:15 -0500

    Problema C

---
+++ src/P2.java
@@ -0,0 +1,107 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P2 {
+	public static String NOT_FOUND = "*";
+	
+	public static void close(BufferedReader br) {
+		try {
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static int sortedIndex(int[] n) {
+		for (int i = 0, prev = 0; i < n.length; i++) {
+			if(prev > n[i]) {
+				return i;
+			} else {
+				prev = n[i];
+			}
+		}
+		return n.length;
+	}
+	
+	public static int[] toIntArray(String n) {
+		int length = n.length();
+		int[] arr = new int[length];
+		for(int i = 0; i < length; i++) {
+			arr[length - 1 - i] = Integer.parseInt(String.valueOf(n.charAt(i)));
+		}
+		return arr;
+	}
+	
+	public static String sp(int[] n, int k) {
+		for(int kActual = 1; kActual <= k; kActual++) {
+			int i = sortedIndex(n);
+			if(i >= n.length) {
+				//Caso donde no existe sp(n,1), puesto que n es el maximo posible.
+				return NOT_FOUND;
+			} else {
+				int[] arr = Arrays.copyOf(n, i);
+				String res = sp(arr, 1);
+				
+				if(res.equals(NOT_FOUND)) {
+					//Caso donde el arreglo[0..i] esta maximizado (no tiene sp con k=1)
+					int replacement = 10;
+					int index = -1;
+					for(int j = 0; j < i; j++) {
+						if(n[i] < n[j] && n[j] < replacement) {
+							index = j;
+							replacement = n[j];
+						}
+					}
+					
+					n[index] = n[i];
+					n[i] = replacement;
+					arr = Arrays.copyOf(n, i);
+					
+					Arrays.sort(arr);
+					
+					for(int j = 0; j < arr.length; j++) {
+						n[j] = arr[arr.length - 1 - j];
+					}
+				} else {
+					//Caso donde el arreglo[0..i] si tiene sp
+					arr = toIntArray(res);
+					
+					for(int j = 0; j < arr.length; j++) {
+						n[j] = arr[j];
+					}
+				}
+			}
+		}
+		
+		String num = "";
+		for(int p = n.length - 1; p >= 0; p--) num += n[p] + "";
+		return num;
+	}
+	
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			
+			String line = br.readLine();
+			while(line != null) {
+				String[] split = line.split(" ");
+				int k = Integer.parseInt(split[0]);
+				int[] n = toIntArray(split[1]);
+				
+				String aws = sp(n,k);
+				System.out.println(aws);
+				
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(br != null) {
+				close(br);
+			}
+		}
+	}
+}
---
+++ src/ProblemaC.java
@@ -0,0 +1,186 @@
+
+
+//David Felipe Niño Romero 				201412734
+//María del Rosario León				201423755
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemaC 
+{
+	private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	private String[] palabras;
+
+	private String parrafo;
+
+	private int anchoMin;
+
+	private int anchoFinal;
+
+	private int rioFinal;
+
+	private ArrayList<String> lineas;
+
+	public ProblemaC()
+	{
+		try 
+		{
+			ingresarParametros();			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void ingresarParametros() throws IOException
+	{
+		parrafo = "";
+		parrafo = in.readLine();
+		while (parrafo != null)
+		{		
+			rioFinal=0;
+
+			anchoFinal=0;
+
+			funcion();
+
+			parrafo = in.readLine();			
+		}	
+	}
+
+	public void funcion()
+	{
+		palabras = parrafo.split(" ");
+
+		palabraMasLarga();
+
+		rioMasLargo();
+
+		System.out.println(""+anchoFinal+" "+rioFinal);
+	}
+
+	public void palabraMasLarga()
+	{
+		int respuesta = -1;
+
+		for(int i=0;i <palabras.length; i++)
+		{
+			if (palabras[i].length()>respuesta)
+			{
+				respuesta = palabras[i].length();
+			}
+		}
+
+		anchoMin =  respuesta;
+		anchoFinal=anchoMin;
+	}
+
+	public void rioMasLargo(){
+
+		int anchoActual =anchoMin;
+
+		int rioActual=0;
+
+		while((parrafo.length()/anchoActual)>1) {
+
+			lineas = new ArrayList<String>();
+
+			int a =partir(anchoActual);
+
+			if(lineas.size()<rioFinal)
+				break;
+
+			rioActual=encontrarRio();
+
+			if(rioActual>rioFinal)
+			{
+				rioFinal=rioActual;
+				anchoFinal= anchoActual;
+			}
+
+			anchoActual+=a;
+		}
+	}
+
+	public int partir(int ancho) {
+		String subParrafo = parrafo;
+		int palabraCorta=Integer.MAX_VALUE;
+		int i=ancho;
+
+		while (subParrafo.length()>i) {
+			if(subParrafo.charAt(i)==' ') 
+			{
+				lineas.add(subParrafo.substring(0,i));
+				subParrafo=subParrafo.substring(i+1);
+			}
+			else if(subParrafo.charAt(i)!=' ')
+			{
+				if(subParrafo.contains(" ")) {
+					int posiblePalabraCorta = subParrafo.substring(i).indexOf(" ");
+					palabraCorta= posiblePalabraCorta<palabraCorta && posiblePalabraCorta>0?posiblePalabraCorta:palabraCorta;
+					i=subParrafo.substring(0, i+1).contains(" ")?subParrafo.substring(0, i+1).lastIndexOf(" "):i;
+					lineas.add(subParrafo.substring(0,i));
+					subParrafo=subParrafo.substring(i+1);
+					i=ancho;
+				}
+			}
+			if(subParrafo.length()<=i && !subParrafo.isEmpty())
+				lineas.add(subParrafo);
+		}
+
+		return palabraCorta;
+
+	}
+
+	public List<Integer> indicesDeEspacios(String linea) {
+		ArrayList<Integer> indices= new ArrayList<>();
+		for (int i = 0; i < linea.length();) {
+			i=linea.indexOf(" ", i+1)!=-1?linea.indexOf(" ", i+1):linea.length();
+			if(i!=linea.length())
+				indices.add(i);
+		}
+		return indices;
+	}
+
+	public int encontrarRio() {
+		int largoMax=1;
+		int largoActual = 1;
+		ArrayList<Integer> indicesActuales = new ArrayList<>();
+		for (int i = 0; i+1 < lineas.size(); i++) {
+			if(indicesActuales.isEmpty())
+			{
+				largoActual=1;
+				indicesActuales=(ArrayList<Integer>) compararEspacios(indicesDeEspacios(lineas.get(i)), indicesDeEspacios(lineas.get(i+1)));
+			}
+			else if(indicesActuales.size()>=1)
+			{
+				largoActual++;
+				indicesActuales=(ArrayList<Integer>) compararEspacios(indicesActuales, indicesDeEspacios(lineas.get(i+1)));
+			}
+
+			if(largoMax<largoActual)
+				largoMax=largoActual;
+		}
+		return largoMax;
+	}
+
+	public List<Integer> compararEspacios(List<Integer> lista, List<Integer> lista2)
+	{
+		ArrayList<Integer> respuesta = new ArrayList<>();
+		for (Integer integer : lista2) {
+			if(lista.contains(integer)||lista.contains(integer+1)||lista.contains(integer-1))
+				respuesta.add(integer);
+		}
+		return respuesta;
+	}
+
+	public static void main(String args[])	
+	{		
+		new ProblemaC();		
+	}
+}
